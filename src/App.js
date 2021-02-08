a8997f3d6e3b7a5268b6c200525350d739580eb4
Ameet Toor <Ameet_Toor@hotmail.com>
2021-02-07 21:22:18 -0800

    Re-adding project files that I deleted by accident as well as modifying .gitignore for MacOS and WebStorm IDE.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import AboutPage from './aboutPage/aboutPage.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <p>Header</p>
+        </div>
+      </header>
+      <AboutPage />
+    </div>
+  );
+}
+
+export default App;
