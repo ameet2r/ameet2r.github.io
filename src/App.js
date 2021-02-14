db638b0c747e5c5b253c8441cbd68639a9942d5f
Ameet Toor <Ameet_Toor@hotmail.com>
2021-02-13 18:30:09 -0800

    Incorporating Blog into App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
-import AboutPage from './aboutPage/aboutPage.js';
+// import AboutPage from './aboutPage/aboutPage.js';
+import Blog from "./Blog.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>
-          <p>Header</p>
-        </div>
-      </header>
-      <AboutPage />
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <div>
+    //       <p>Header</p>
+    //     </div>
+    //   </header>
+    //   <AboutPage />
+    // </div>
+    <Blog />
   );
 }
 
