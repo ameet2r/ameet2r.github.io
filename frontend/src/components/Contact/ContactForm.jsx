602f4418ab5319f1938bd79ec64c26c82df7929e
Ameet Toor <ameet2r@users.noreply.github.com>
2025-09-17 00:32:57 -0700

    Remove logging

--- frontend/src/components/Contact/ContactForm.jsx
+++ frontend/src/components/Contact/ContactForm.jsx
@@ -101,8 +101,6 @@ const ContactForm = () => {
     }
 
     try {
-      console.log(import.meta.env.VITE_BACKEND_API);
-      console.log(`${import.meta.env.VITE_BACKEND_API}/contact`);
       const response = await fetch(`${import.meta.env.VITE_BACKEND_API}/contact`, {
         method: 'POST',
         headers: {
